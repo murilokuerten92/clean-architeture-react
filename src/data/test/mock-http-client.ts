5aa0b5338a5aabd52636777f6051ab914e634b1c
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2021-04-30 20:48:57 -0300

    refactor: organize files and folders

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from './../usecases/authentication/protocols/http/http-post-client';
+import { HttpPostClient } from '../protocols/http/http-post-client';
 
 export class HttpPostClientSpy implements HttpPostClient {
     url?: string;
