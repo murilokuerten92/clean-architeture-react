c6958331bfa28085d0321d61470f2563ac3a23f5
Murilo Kuerten <murilo.kuerten@vialaserdepilacao.com.br>
2021-05-10 22:11:32 -0300

    test: ensure RemoteAuthentication return an Account on success

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,6 +1,6 @@
 import { UnexpectedError } from '@/domain/errors/unexpected-error';
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error';
-import { mockAuthentication } from "@/domain/test/mock-authentication";
+import { mockAccountModel, mockAuthentication } from "@/domain/test/mock-authentication";
 import { HttpPostClientSpy } from "@/data/test/mock-http-client";
 import { RemoteAuthentication } from "./remote-authentication";
 import { HttpStatusCode } from '@/data/protocols/http/http-response';
@@ -70,5 +70,16 @@ describe("RemoteAuthentication", () => {
     }
     const promise = sut.auth(mockAuthentication());
     await expect(promise).rejects.toThrow(new UnexpectedError());
+  });
+
+  test("Should return an AccountModel if HttpPostClient returns 200", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+    const httpResult = mockAccountModel();
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: httpResult
+    }
+    const account = await sut.auth(mockAuthentication());
+    expect(account).toEqual(httpResult);
   });
 });
